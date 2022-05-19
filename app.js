34f20f76d5515692b0a302c92abd3b16ceea8a4c
rob oren <acalltoredemption@gmail.com>
2022-05-19 12:44:11 -0400

    connecting our backend to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const req = require('express/lib/request');
+//Connect to MongoDB
+const dbURI = 'mongodb+srv://Stino:kkkkkkk7@cluster0.oy23c.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 //express app
 const app = express();
@@ -7,22 +16,32 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-// listen for requests
-app.listen(3000);
+//middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+
+
 
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'},
-    {title: 'How to defeat Bowser', snippet: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'},
-  ];
-   res.render('index', {title: 'Home', blogs});
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   //  res.send('<p>about page</p>');
   res.render('about', {title: 'About'});
 });
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({createdAt: -1})
+  .then((result) => {
+    res.render('index', {title: 'All Blogs', blogs: result})
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', {title: 'Create'});
